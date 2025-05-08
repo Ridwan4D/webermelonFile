2686717f9805d0da3aa40bded2ae12ec4702a76e
Ridwan4D <ridwnaul161@gmail.com>
2025-05-08 18:58:50 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const professionToggleBtn = document.querySelector("#professionToggleBtn");
+const industryToggleBtn = document.querySelector("#industryToggleBtn");
+const professionForm = document.querySelector("#professionForm");
+const industryFilterBox = document.querySelector("#industryFilter");
+
+
+// professionToggleBtn.addEventListener("click", () => {
+//     professionForm.style.height = "0";
+// })
